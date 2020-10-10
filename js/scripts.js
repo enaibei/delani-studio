c89bc50c1fbe77b86ffae091557a3ea8fbab77d1
Esther.Naibei <esthernaibei2020@gmail.com>
2020-10-10 17:18:14 +0300

    add the portfolio text and hover effects

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,57 @@ $(document).ready(function(){
         $(".panel3").slideToggle();
     })
 })
+
+
+$(document).ready(function(){
+    $("#work1").mouseover(function(){
+      $("#portfolio1").show();
+    }).mouseout(function(){
+      $("#portfolio1").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work2").mouseover(function(){
+      $("#portfolio2").show();
+    }).mouseout(function(){
+      $("#portfolio2").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work3").mouseover(function(){
+      $("#portfolio3").show();
+    }).mouseout(function(){
+      $("#portfolio3").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work4").mouseover(function(){
+      $("#portfolio4").show();
+    }).mouseout(function(){
+      $("#portfolio4").hide();
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#work5").mouseover(function(){
+      $("#portfolio5").show();
+    }).mouseout(function(){
+      $("#portfolio5").hide();
+    });
+    $("#work6").mouseover(function(){
+      $("#portfolio6").show();
+    }).mouseout(function(){
+      $("#portfolio6").hide();
+    });
+    $("#work7").mouseover(function(){
+      $("#portfolio7").show();
+    }).mouseout(function(){
+      $("#portfolio7").hide();
+    });
+    $("#work8").mouseover(function(){
+      $("#portfolio8").show();
+    }).mouseout(function(){
+      $("#portfolio8").hide();
+    });
+  });
+  
