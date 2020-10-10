99030249a2adcf1aaf0574503ad2700f0dc52eee
Esther.Naibei <esthernaibei2020@gmail.com>
2020-10-10 13:09:32 +0300

    add the toggle slide to the what we do section

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".flip").click(function(){
+        $(".panel").slideToggle();
+    })
+})
+$(document).ready(function(){
+    $(".flip2").click(function(){
+        $(".panel2").slideToggle();
+    })
+})
+$(document).ready(function(){
+    $(".flip3").click(function(){
+        $(".panel3").slideToggle();
+    })
+})
